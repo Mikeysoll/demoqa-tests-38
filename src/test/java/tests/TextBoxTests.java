c0206d9043bf08597661ba86708baad3d33354c9
Mikhail Solovyov <solovev02@yandex.ru>
2025-10-07 21:14:18 +0300

    added CssXpathExamples

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -1,4 +1,6 @@
-    import com.codeborne.selenide.Configuration;
+package tests;
+
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
